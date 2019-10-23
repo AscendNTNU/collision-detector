69f4eaf1b26db526db286fbd6d499fc9e82670ef
mcallistertyler95 <tytheguy95@gmail.com>
2019-10-23 15:42:20 +0200

    Added jsk text for rviz display.

--- scripts/collision_detector.py
+++ scripts/collision_detector.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String, Empty, Bool, Int32
+from jsk_rviz_plugins.msg import OverlayText
 
 num_collisions = 0
-collision_pub = rospy.Publisher('/viz/collision', Bool, queue_size=10)
-num_collision_pub = rospy.Publisher('/viz/num_collisions', Int32, queue_size=10, latch=True)
+collision_pub = rospy.Publisher('/viz/collision', OverlayText, queue_size=10)
+num_collision_pub = rospy.Publisher('/viz/num_collisions', OverlayText, queue_size=10, latch=True)
 def collision_callback(data):
     global num_collisions
     num_collisions = num_collisions + 1
-    collision_pub.publish(Bool(True))
-    num_collision_pub.publish(Int32(num_collisions))
+    has_collided = OverlayText(text=str('True'))
+    collision_pub.publish(has_collided)
+    text_collisions = OverlayText(text=str(num_collisions) + ' collision(s)')
+    num_collision_pub.publish(text_collisions)
 
 if __name__ == '__main__':
     rospy.init_node('listener', anonymous=True)
